ecfa06b2048a8cbdd8b3ccb842469f9b381c026b
Shantanu Kulkarni <shantanukulkarni1229@gmail.com>
2024-12-09 11:22:56 +0530

    Chnages in the Logo

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -61,13 +61,13 @@ const SignUpPage = () => {
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
-			<div className='flex-1 hidden lg:flex items-center  justify-center'>
-				<XSvg className='lg:w-2/3 fill-white' />
+			<div className='flex-1 hidden lg:flex items-center justify-center'>
+				<img src='./posts/cw.png' alt='My Logo' className='lg:w-2/3' />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<XSvg className='w-24 lg:hidden fill-white' />
-					<h1 className="text-5xl font-extrabold text-white tracking-tight">
+					<img src='./posts/cw.png' alt='My Logo' className='lg:w-2/3' />
+										<h1 className="text-5xl font-extrabold text-white tracking-tight">
 						Enter the Verse
 					</h1>
 					<p className="text-lg font-medium text-gray-300 max-w-2xl mx-auto">
