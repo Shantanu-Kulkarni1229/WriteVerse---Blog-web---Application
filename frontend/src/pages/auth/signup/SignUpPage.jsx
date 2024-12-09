6892099bd832d9bbbc1a2121fec3b4a622361e2e
Shantanu Kulkarni <shantanukulkarni1229@gmail.com>
2024-12-09 11:51:26 +0530

    `Removed redundant logo image in SignUpPage component`

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -61,12 +61,12 @@ const SignUpPage = () => {
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen px-10'>
-			{/* <div className='flex-1 hidden lg:flex items-center justify-center'>
+			<div className='flex-1 hidden lg:flex items-center justify-center'>
 				<img src='./posts/cw.png' alt='My Logo' className='lg:w-2/3' />
-			</div> */}
+			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
 				<form className='lg:w-2/3  mx-auto md:mx-20 flex gap-4 flex-col' onSubmit={handleSubmit}>
-					<img src='./posts/cw.png' alt='My Logo' className='lg:w-2/3' />
+					{/* <img src='./posts/cw.png' alt='My Logo' className='lg:w-2/3' /> */}
 										<h1 className="text-5xl font-extrabold text-white tracking-tight">
 						Enter the Verse
 					</h1>
